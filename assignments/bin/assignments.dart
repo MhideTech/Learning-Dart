7855f41f2c9a760d9e9c1e9eb7c4afe12c6fbf8e
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-10-30 15:14:35 +0100

    Started my third assignment of creating 15 functions with different parameters

--- assignments/bin/assignments.dart
+++ assignments/bin/assignments.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   print('Hello world');
 
@@ -104,10 +106,93 @@ void main() {
   // What is the late keyword and how is it used with nullables?
   // the late keyword is used to declare a variable that is initially uninitialized but is guaranteed to be assigned a non-null value before it's accessed.
 
-
   /* 
     11 October 2023 
     1. Create 15 functions of required named, optional named and required positional parameters
     2. Recharge card with if-else and switch statement
   */
+
+  // printOrder(item: "Widget", price: 12.99);
+
+  // printGreeting("Alice"); // Uses the default title.
+  // printGreeting("Bob", "Dr."); // Provides a custom title.
+
+  // sendMail(
+  //     to: 'oladipupoibrahim20@gmail.com',
+  //     subject: 'Application open for Java Developer',
+  //     body:
+  //         'We are in need of a Java Developer with experience with Spring Boot. If this mail finds you well, please do well to send your resume.');
+
+  // order(item: 'Laptop', quantity: 5);
+
+  // areaOfARectangle(10, 5);
+
+
+}
+
+void printOrder({String? item, int? quantity, double? price}) {
+  print("Order Details:");
+  if (item != null) {
+    print("Item: $item");
+  }
+  if (quantity != null) {
+    print("Quantity: $quantity");
+  }
+  if (price != null) {
+    print("Price: \$$price");
+  }
+}
+
+void printGreeting(String name, [String? title = "Mr."]) {
+  print("Hello, $title $name!");
+}
+
+void sendMail({required String to, String? subject, required String body}) {
+  if (subject != null) {
+    print(
+        'Mail: $body with a subject of $subject has been sent to $to successfully!');
+  } else {
+    print('$body has been sent to $to successfully!');
+  }
+}
+
+void order({required String item, required int quantity}) {
+  print(
+      'Order complete!. $quantity pieces of $item has been ordered successful. Please wait while we process your delivery...');
+}
+
+void register(
+    {required String username,
+    required String email,
+    required String password}) {
+  print(
+      "Registration successful! Your informations are Username: $username, Email: $email, Password: $password. Thanks for creating a profile with us.");
+}
+
+void orderPizza(String type, String size, bool? extraCheese) {
+  print('A $size size pizza with type $type has been placed.');
+}
+
+void post(String text) {
+  print('$text post has been made successfully');
+}
+
+void bookings(String eventTitle, String date, int numberOfTickets) {
+  print(
+      'Mhizta Orlah hotel has been booked for $date with $numberOfTickets tickets for a/an $eventTitle occassion');
+}
+
+int areaOfARectangle(int length, int breadth) {
+  int area = length * breadth;
+  print(
+      'Area of a rectangle with sides $length and $breadth is equals to $area');
+  return 1;
+}
+
+void greeting(String name, {String? greeting}) {
+  if (greeting != null) {
+    print('$greeting $name 👋👋👋');
+  } else {
+    print('Hello $name. It\'s nice to have you');
+  }
 }
